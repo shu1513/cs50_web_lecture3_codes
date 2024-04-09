2fbae5284060e3ad7d70483648a4e5a5935ff1b1
Shu Yang <shu@Shus-MBP.lan>
2024-04-08 20:00:03 -0700

    upload codes for Django lecture CS50

---
+++ tasks/views.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+# Create your views here.
+tasks = ["foo", "bar", "baz"]
+
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="priority", min_value=1, max_value=5)
+
+
+def index(request):
+    return render(request, "tasks/index.html", {"tasks": tasks})
+
+
+def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/index.html", {"form": form})
+
+    return render(request, "tasks/add.html", {"form": NewTaskForm()})
